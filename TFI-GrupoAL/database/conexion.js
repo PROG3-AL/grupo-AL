66950eb146044b5403555ddca509d82634ab04f2
Demian Diak <Demiandiak@gmail.com>
2025-09-07 13:59:14 -0300

    cree estructura de carpetas

---
+++ TFI-GrupoAL/database/conexion.js
@@ -0,0 +1,27 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+export async function getTodosLosSalones() {
+    try {
+      const conexion = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      });
+  
+      const sqlQuery = 'SELECT * FROM salones';
+  
+      const [rows] = await conexion.query(sqlQuery);
+  
+      console.log('Query results:', rows);
+  
+      await conexion.end();
+  
+    } catch (err) {
+      console.error('Error executing SELECT query:', err);
+    }
+  }
+  
