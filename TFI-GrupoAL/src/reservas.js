4f8ea884b83d66d2f02fe2e7985a4b73218bb419
Micaela O. <micaelagiselleortiz0@gmail.com>
2025-11-05 10:56:11 -0300

    limpieza de archivos y comentarios

--- TFI-GrupoAL/src/reservas.js
+++ TFI-GrupoAL/src/reservas.js
@@ -3,12 +3,6 @@ import { router as v1SalonesRutas } from './v1/routes/salonesRouter.js';
 import { router as v1UsuariosRutas } from './v1/routes/usuariosRouter.js'; //importamos rutas de usuarios
 import { router as v1ReservasRutas } from './v1/routes/reservasRouter.js';
 import { router as v1ServiciosRutas } from './v1/routes/serviciosRouter.js';
-import expressHandlebars from 'express-handlebars';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path'; 
-import nodemailer from "nodemailer";
-import { readFile } from "fs/promises";
-import handlebars from "handlebars";
 import morgan from 'morgan';
 import fs from 'fs';
 import { router as v1TurnosRutas } from "./v1/routes/turnosRouter.js";
@@ -20,22 +14,6 @@ app.use(express.json(
   {type: 'application/json'}
 ));
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const hbs = expressHandlebars.create({
-  defaultLayout: "main",
-  layoutsDir: join(__dirname, "views", "layouts"),
-  helpers: {
-    eq: (arg1, arg2) => arg1 === arg2,
-  },
-})
-
-app.engine("handlebars", hbs.engine)
-app.set("view engine", "handlebars")
-app.set("views", join(__dirname, "views", "pages"))
-
-app.use(express.static(join(__dirname, "..", "public")))
 
 app.get('/estado', (req, res) => {
   res.json({'ok':true})
